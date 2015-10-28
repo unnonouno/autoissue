8f720dac5d7894d17cc119dcecb33ba73228a5c6
Yuya Unno <unnonouno@gmail.com>
2015-10-28 16:51:38 +0900

    Use milestone title

--- autoissue.py
+++ autoissue.py
@@ -6,23 +6,42 @@ import sys
 
 import requests
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--owner', '-o', required=True, help='Owner name')
-parser.add_argument('--repo', '-r', required=True, help='Repository name')
-parser.add_argument('--milestone', '-m', required=True, type=int, help='Milestone number')
-args = parser.parse_args()
 
-issues = collections.defaultdict(list)
-page = 1
-while True:
-    url = 'https://api.github.com/repos/{0}/{1}/issues?milestone={2}&state=close&page={3}'.format(args.owner, args.repo, args.milestone, page)
+def download_json(url):
     r = requests.get(url)
 
     if r.status_code != 200:
         print r.json()
         sys.exit(1)
 
-    js = r.json()
+    return r.json()
+
+
+def find_milestone(milestone):
+    url = 'https://api.github.com/repos/{0}/{1}/milestones'.format(args.owner, args.repo)
+    js = download_json(url)
+    for m in js:
+        if m['title'] == args.milestone:
+            return m['number']
+
+    print('milestone is not found: {}'.format(milestone))
+    sys.exit(1)
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--owner', '-o', required=True, help='Owner name')
+parser.add_argument('--repo', '-r', required=True, help='Repository name')
+parser.add_argument('--milestone', '-m', required=True, help='Milestone title')
+args = parser.parse_args()
+
+issues = collections.defaultdict(list)
+page = 1
+
+milestone_number = find_milestone(args.milestone)
+
+while True:
+    url = 'https://api.github.com/repos/{0}/{1}/issues?milestone={2}&state=close&page={3}'.format(args.owner, args.repo, milestone_number, page)
+    js = download_json(url)
     if len(js) == 0:
         break
     for issue in js:
